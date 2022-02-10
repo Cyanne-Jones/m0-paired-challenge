93d4d3ec21ef44375e131f697393f8b43d06ee33
Cyanne Jones <jonescyanne@yahoo.com>
2022-02-09 20:24:01 -0700

    submit challenge

---
+++ challenge/index.md
@@ -0,0 +1,8 @@
+# Book Titles
+- Dune
+- The Adventures of Ibn-Battuta
+- King City
+- War All The Time
+- Aloud
+
+In this challenge we both drove and navigated. When we had trouble, we would ask each other for help and if what we did was working. I got stuck on just thinking of content for these variables quickly! I had to look up if a cloned repository already had git running in it (the answer was yes!).
---
+++ challenge/library-challenge.js
@@ -0,0 +1,13 @@
+// I chose library-challenge.js for my title because this challenge was book/library centric and it felt fitting.
+var bestBook = "Dune";
+// this variable declares that the best book is absolutely Dune.
+var releaseDate = 1965;
+//It felt better to store this as a number, as I wanted to not use a string. 
+var frankHerbertAuthor = true;
+// This variable is the answer to the question if Frank Herbert is the author of Dune.
+var numPages = 963;
+var pageMaterial = "Paper";
+var coverStyle = "Hardback";
+
+var duneSeries = ["Dune", "Dune Messiah", "Children of Dune", "God Emperor of Dune"];
+// This is not an exhaustive list of the books in the series, but the ones I have sitting on my shelf right now.
